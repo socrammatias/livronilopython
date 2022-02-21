43f2d80282ba774269dfcd4f956b7e71fb54d03f
tarkerin <feelthegamesense@gmail.com>
2022-02-21 10:35:27 -0300

    Maneira do nilo de fazer bubble sort no livro

---
+++ capitulo6/bubble_sort_do_n.py
@@ -0,0 +1,17 @@
+l = [7, 4, 3, 12, 8]
+fim = 5
+while fim > 1:
+    trocou = False
+    x = 0
+    while x < (fim - 1):
+        if l[x] > l[x + 1]:
+            trocou = True
+            temp = l[x]
+            l[x] = l[x + 1]
+            l[x + 1] = temp
+        x += 1
+    if not trocou:
+        break
+    fim -= 1
+for e in l:
+    print(e)
