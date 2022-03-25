22087de31470830cfb86013e179d5e0b153a2dec
tarkerin <feelthegamesense@gmail.com>
2022-03-24 21:58:49 -0300

    aa

---
+++ capitulo9/exercicio_9.29.py
@@ -0,0 +1,25 @@
+# Gerando pagina web a partir de um dicionario
+
+filmes = {
+    'drama': ['Cidadao kane', 'O Poderoso Chefao'],
+    'comedia': ['Tempos modernos', 'American Pie', 'Dr. Dolittle'],
+    'policial': ['Chuva Negra', 'Desejo de matar', 'Dificil de matar'],
+    'guerra': ['Rambo', 'Platoon', 'Tora!Tora!Tora!']
+}
+
+with open('filmes.html', 'w', encoding='utf-8') as pagina:
+    pagina.write('''
+!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title>filmes</title>
+</head>
+<body> 
+    ''')
+    for c, v in filmes.items():
+        pagina.write('<h1>{}</h1>\n'.format(c))
+        for e in v:
+            pagina.write('<p>{}</p>\n'.format(e))
+    pagina.write('</body></html>')
+pagina.close()
