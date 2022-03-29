739f5aabe4e695a0221a604cc34b1a63f40bd5ce
tarkerin <feelthegamesense@gmail.com>
2022-03-29 11:04:14 -0300

    10.6

---
+++ capitulo10/exercicio_10.9.py
@@ -0,0 +1,7 @@
+class estados():
+    def __init__(self, sigla, nome):
+        self.sigla = sigla
+        self.nome = nome
+    def soma_populacao(self, populacao):
+        self.populacao += populacao
+        
